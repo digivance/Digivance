3978d0301eb98414cf2f4edb0b7a6885da12e0a2
Dan Mayor <dmayor@digivance.com>
2025-01-05 11:28:11 -0500

    Introducing BaseModel (#2)

---
+++ Digivance.Data/Models/BaseModel.cs
@@ -0,0 +1,34 @@
+﻿namespace Digivance.Data.Models
+{
+    /// <summary>
+    /// BaseModel represents the shared id and auditing fields of models within
+    /// the Digivance Platform family of microservices.
+    /// </summary>
+    public record BaseModel
+    {
+        /// <summary>
+        /// Optional unique id of the entity that created this record
+        /// </summary>
+        public Guid? CreatedBy { get; set; }
+
+        /// <summary>
+        /// UTC date and time when this record was first created
+        /// </summary>
+        public DateTime CreatedOn { get; set; }
+
+        /// <summary>
+        /// The unique id of this record
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Optional unique id of the entity that last modified this record
+        /// </summary>
+        public Guid? ModifiedBy { get; set; }
+
+        /// <summary>
+        /// Optional UTC date and time when this record was last modified
+        /// </summary>
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
