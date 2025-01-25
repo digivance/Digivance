b05b5dbfbfa496d59ee586ef29653b9b8d2b7200
Dan Mayor <dmayor@digivance.com>
2025-01-25 08:48:09 -0500

    Adding EF package (#9)
    * Adding EF package
    
    * Adding EF package

---
+++ Digivance.Data.EntityFramework.Tests/Contexts/SampleContext.cs
@@ -0,0 +1,50 @@
+﻿using Digivance.Data.EntityFramework.Tests.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Digivance.Data.EntityFramework.Tests.Contexts
+{
+    /// <summary>
+    /// Very simple in memory context we use to test our BaseEntity
+    /// </summary>
+    public class SampleContext : DbContext
+    {
+        /// <summary>
+        /// Represents the SampleEntities collection
+        /// </summary>
+        public DbSet<SampleEntity> SampleEntities { get; set; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public SampleContext() { }
+
+        /// <summary>
+        /// Basic constructor, instantiate from provided DbContextOptions
+        /// </summary>
+        /// <param name="options">The options to instantiate this context with</param>
+        public SampleContext(DbContextOptions<SampleContext> options) : base(options) { }
+
+        /// <summary>
+        /// Default OnConfiguring will setup our basic in memory context with the
+        /// name "TEST_SAMPLE"
+        /// </summary>
+        /// <param name="optionsBuilder">The options builder we can configure</param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("TEST_SAMPLE");
+        }
+
+        /// <summary>
+        /// Default OnModelCreating will configure the entity types we are using in
+        /// collections above
+        /// </summary>
+        /// <param name="modelBuilder">The model builder we can configure</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            new SampleEntityConfiguration()
+                .Configure(modelBuilder.Entity<SampleEntity>());
+        }
+    }
+}
---
+++ Digivance.Data.EntityFramework.Tests/Models/SampleModel.cs
@@ -0,0 +1,15 @@
+﻿using Digivance.Data.Models;
+
+namespace Digivance.Data.EntityFramework.Tests.Models
+{
+    /// <summary>
+    /// Sample Model that we use to test our BaseEntity with
+    /// </summary>
+    public record SampleModel : BaseModel
+    {
+        /// <summary>
+        /// A custom field on our sample entity
+        /// </summary>
+        public string? SampleMessage { get; set; }
+    }
+}
