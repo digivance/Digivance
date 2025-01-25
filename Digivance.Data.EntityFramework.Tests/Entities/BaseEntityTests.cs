e46ca2affe956d4b623857601de485111c266dbd
Dan Mayor <dmayor@digivance.com>
2025-01-25 08:25:42 -0500

    Adding EF package

---
+++ Digivance.Data.EntityFramework.Tests/Entities/BaseEntityTests.cs
@@ -0,0 +1,42 @@
+﻿using Digivance.Data.EntityFramework.Tests.Contexts;
+using Digivance.Data.EntityFramework.Tests.Models;
+
+namespace Digivance.Data.EntityFramework.Tests.Entities
+{
+    /// <summary>
+    /// Tests to ensure BaseEntity functions as expected
+    /// </summary>
+    [TestFixture]
+    public class BaseEntityTests
+    {
+        /// <summary>
+        /// Ensures we can cast to a custom model that inherits from BaseModel
+        /// </summary>
+        [Test]
+        public void Can_CastToModel()
+        {
+            var entity = new SampleEntity { Id = Guid.NewGuid() };
+            var model = entity.ToBaseModel<SampleModel>();
+
+            Assert.That(model.Id, Is.EqualTo(entity.Id));
+        }
+
+        /// <summary>
+        /// Ensures that our ConfigureBaseEntity() extension works as expected
+        /// </summary>
+        [Test]
+        public void Can_ConfigureType()
+        {
+            var context = new SampleContext();
+
+            var entity = new SampleEntity { SampleMessage = "Hello World" };
+            context.SampleEntities.Add(entity);
+            context.SaveChanges();
+
+            var dataRow = context.SampleEntities
+                .FirstOrDefault(x => x.SampleMessage == entity.SampleMessage);
+
+            Assert.That(dataRow, Is.EqualTo(entity));
+        }
+    }
+}
---
+++ Digivance.Data.EntityFramework.Tests/Entities/SampleEntity.cs
@@ -0,0 +1,32 @@
+﻿using Digivance.Data.EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Digivance.Data.EntityFramework.Tests.Entities
+{
+    /// <summary>
+    /// A sample entity we use to test our BaseEntity functionality
+    /// </summary>
+    public record SampleEntity : BaseEntity
+    {
+        /// <summary>
+        /// A custom field on our sample entity
+        /// </summary>
+        public string? SampleMessage { get; set; }
+    }
+
+    /// <summary>
+    /// EntityFramework type configuration for our sample entity
+    /// </summary>
+    public class SampleEntityConfiguration : IEntityTypeConfiguration<SampleEntity>
+    {
+        public void Configure(EntityTypeBuilder<SampleEntity> builder)
+        {
+            builder.ConfigureBaseEntity();
+
+            builder.Property(x => x.SampleMessage)
+                .IsRequired(false)
+                .HasMaxLength(255);
+        }
+    }
+}
